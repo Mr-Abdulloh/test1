39f134a677dca8e11f67045aa1610c83128df5d7
abdulloh muradullaev <muradullaevabdulloh4002@gmail.com>
2023-06-17 10:56:17 +0500

    Hello man Wlecome to my first programm

--- first_git.py
+++ first_git.py
@@ -1,3 +1,12 @@
-print("Hello there")
-print("thei is my first programm in Git hub")
-
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        sun=0
+        for i in range(len(nums)):
+            if nums.count(0)==len(nums):
+                break
+            a=min(list(filter(lambda x:x,nums)))
+            sun+=1
+            for k in range(len(nums)):
+                if nums[k]!=0:
+                    nums[k]-=a
+        return sun
